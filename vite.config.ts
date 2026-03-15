64c1cf73e171d99c97403ae27359c0aed73b61ee
Jal821 <69259126+Jal821@users.noreply.github.com>
2026-03-15 12:58:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cocktails4you/",
   server: {
     host: "::",
     port: 8080,
